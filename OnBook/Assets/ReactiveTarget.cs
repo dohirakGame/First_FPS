54e7cf1032c224bc92ff0087fd2c2f797984dece
Arthur <ar--r@mail.ru>
2021-09-15 01:34:51 +0300

    All project

---
+++ OnBook/Assets/ReactiveTarget.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class ReactiveTarget : MonoBehaviour
+{
+    void Start()
+    {
+        WanderingAI behavior = GetComponent<WanderingAI>();
+        if (behavior != null)
+        {
+            behavior.SetAlive(false);
+        }
+    }
+
+    public void ReactToHit()
+    {
+        
+        StartCoroutine(Die());
+    }
+
+    private IEnumerator Die()
+    {
+        this.transform.Rotate(-75,0,0);
+
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(this.gameObject);
+    }
+}
